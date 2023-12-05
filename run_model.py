303971df94d8719e1d2ded8361dbcf74106f4256
Han Qi <qihan.dev@gmail.com>
2023-12-05 05:59:42 +0000

    update

--- run_model.py
+++ run_model.py
@@ -44,7 +44,7 @@ def unwrap(torchtensors):
     return tree_map_only(JaxTensor, lambda x: x._elem, torchtensors)
 
 
-def main2(jitted_block=False, fori_loop=False):
+def main2(jit=True):
     sample_args = (
         torch.randint(0, 32000, (1, 2048)),
         torch.arange(0, 2048),
@@ -52,7 +52,7 @@ def main2(jitted_block=False, fori_loop=False):
     sample_args = tree_map(move_to_device, sample_args)
     model_args = ModelArgs(**transformer_configs['7B'])
     # model_args.n_layer = 2
-    m = Transformer(model_args, jitted_block, fori_loop)
+    m = Transformer(model_args)
     m.to(torch.bfloat16)
     m.setup_caches(1, 2048)
     m_func, weights, buffer = make_functional_with_buffers(m)
@@ -60,16 +60,12 @@ def main2(jitted_block=False, fori_loop=False):
     causal_mask = move_to_device(m.causal_mask)
     freqs_cis = move_to_device(m.freqs_cis)
 
-    if fori_loop:
-        m_func.stateless_model._stacked_buffers = tree_map_only(torch.Tensor, move_to_device, m._stacked_buffers)
-        m_func.stateless_model._stacked_weights = tree_map_only(torch.Tensor, move_to_device, m._stacked_weights)
     weights = tree_map(move_to_device, weights)
     buffer = tree_map(move_to_device, buffer)
 
 
 
 
-    @jax.jit
     def m_func_jit(
         weights, buffer, args, causal_mask, freqs_cis
     ):
@@ -82,6 +78,8 @@ def main2(jitted_block=False, fori_loop=False):
         res = unwrap(res)
         return res
 
+    if jit:
+        m_func_jit = jax.jit(m_func_jit)
     args = weights, buffer, sample_args, causal_mask, freqs_cis 
     args = unwrap(args)
     # print(m_func_jit.lower(*args).as_text())
@@ -96,11 +94,9 @@ if __name__ == '__main__':
     if sys.argv[1] == 'torch':
         main()
     elif sys.argv[1] == 'jax':
-        main2()
-    elif sys.argv[1] == 'jax_block':
-        main2(True, False)
-    elif sys.argv[1] == 'jax_fori':
-        main2(False, True)
+        main2(False)
+    elif sys.argv[1] == 'jax_jit':
+        main2(True)
 
 
     
